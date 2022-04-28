b3542397fad0258f54d7692e4004c991590fecf8
yatou0912 <59738412+yatou0912@users.noreply.github.com>
2022-04-28 21:45:36 +0800

    Create plus.c
    c program ,scanf use

---
+++ plus.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("please input two num\n");
+    scanf("%d,%d",&a,&b);
+    c=a+b;
+    printf("%d+%d=%d",a,b,c);
+
+}
