0c4e40672c1f685b8b4ec0fa7eb6ebc27cd475a5
yatou0912 <59738412+yatou0912@users.noreply.github.com>
2022-04-29 09:48:37 +0800

    Update plus.c

--- plus.c
+++ plus.c
@@ -5,7 +5,7 @@ int main()
     int b;
     int c;
     printf("please input two num\n");
-    scanf("%d,%d",&a,&b);
+    scanf("%d,%d",&a,&b);/*  此处输入的时候需注意 ，不能忽略逗号，比如输入两个数字 1  2，则这样输入1,2  */
     c=a+b;
     printf("%d+%d=%d",a,b,c);
 
